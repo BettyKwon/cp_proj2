fa7e91906390bcf7c6c93404aaf85737e9b5ff65
BettyKwon <kbk2289@gmail.com>
2016-12-09 00:25:02 +0900

    new checkmate : need to debug on other machines

--- src/chess/ChessBoard.java
+++ src/chess/ChessBoard.java
@@ -223,9 +223,12 @@ public class ChessBoard {
 				System.out.println(selectedPiece.type);
 				System.out.println(selectedPiece.color);
 				isCheck = false;
-				markPossibleMoves(selectedPiece.type, selectedPiece.color, x, y);
+				isCheckMate = false;
+				isCheck = check();
+				//mark(getAreaToAttack(selectedPiece.type, selectedPiece.color, x, y));
+				//markPossibleMoves(selectedPiece.type, selectedPiece.color, x, y);
 				//checkStatus(selectedPiece.type, selectedPiece.color, x, y);
-				if(isCheck) checkMate();
+				if(isCheck) isCheckMate = checkmate1();
 				unmarkAll();
 				toggleTurn();
 			}
@@ -233,11 +236,121 @@ public class ChessBoard {
 				selectedPiece = getIcon(x, y);
 				checkTest = false;
 				unmarkAll();
-				if(selectedPiece.color == turn) 
-					markPossibleMoves(selectedPiece.type, selectedPiece.color, x, y);
+				if(selectedPiece.color == turn) {
+					mark(getAreaToAttack(selectedPiece.type, selectedPiece.color, x, y, turn));
+					mark(getAreaToMove(selectedPiece.type, selectedPiece.color, x, y, turn));
+					//markPossibleMoves(selectedPiece.type, selectedPiece.color, x, y);
+				}
 			}
 			xPrev = x;
 			yPrev = y;
+		}
+		public boolean check() {
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					Piece p = getIcon(i, j);
+					if(p.color == turn) {
+						if(contain(PieceType.king, adversary, getAreaToAttack(p.type, p.color, i, j, turn))) {
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
+		public boolean contain(PieceType type, PlayerColor color, Vector<Vector<Tuple<Integer, Integer>>> area) {
+			for(Vector<Tuple<Integer, Integer>> v : area) {
+				for(Tuple<Integer, Integer> t : v) {
+					Piece p = getIcon(t.first(), t.second()); 
+					if(p.type == type && p.color == color) {
+							return true;
+					}
+				}
+			}
+			return false;
+		}
+		public void print(Vector<Vector<Tuple<Integer, Integer>>> a) {
+			for(Vector<Tuple<Integer, Integer>> v : a) {
+				for(Tuple<Integer, Integer> t : v) {
+					System.out.print(t.first() + " " + t.second() + " / ");
+				}
+			System.out.println();
+			}
+		}
+		public boolean checkmate1() {
+			boolean result;
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					Piece p = getIcon(i, j);
+					if(p.color == adversary) {
+						Vector<Vector<Tuple<Integer, Integer>>> area1 = getAreaToAttack(p.type, p.color, i, j, adversary);
+						Vector<Vector<Tuple<Integer, Integer>>> area2 = getAreaToMove(p.type, p.color, i, j, adversary);
+						area1.addAll(area2);
+						System.out.println("CHECK AREA TO ATTACK -- " + p.type + p.color + " " + i + " " + j);
+						print(area1);
+						for(Vector<Tuple<Integer, Integer>> v : area1) {
+							for(Tuple<Integer, Integer> t : v) {
+								System.out.println("checkmate1 : " + i + " " + j + " " + t.first() + " " + t.second());
+								JButton[][] tempChessBoardSquares = new JButton[8][8];
+								for(int a=0; a<8; a++) {
+									System.arraycopy(chessBoardSquares[a], 0, tempChessBoardSquares[a], 0, 8);
+								}
+								Piece[][] tempChessBoardStatus = new Piece[8][8];
+								for(int a=0; a<8; a++) {
+									System.arraycopy(chessBoardStatus[a], 0, tempChessBoardStatus[a], 0, 8);
+								}
+								movePiece(i, j, t.first(), t.second());
+								result = check();
+								for(int a=0; a<8; a++) {
+									System.arraycopy(tempChessBoardSquares[a], 0, chessBoardSquares[a], 0, 8);
+								}
+								for(int a=0; a<8; a++) {
+									System.arraycopy(tempChessBoardStatus[a], 0, chessBoardStatus[a], 0, 8);
+								}
+								if(!result) {
+									return false;
+								}
+							}
+						}
+					}
+				}
+			}
+			return true;
+		}
+		public boolean checkMate() {
+//			PlayerColor opposite = PlayerColor.none;
+//			if(turn == PlayerColor.black) opposite = PlayerColor.white;
+//			Tuple<Integer, Integer> target = getTarget(PieceType.king, opposite);
+//			// 1. 상대편 왕이 갈 자리가 없을 때
+//			Vector<Vector<Tuple<Integer, Integer>>> kingAttacks = getAreaToAttack(PieceType.king, opposite, target.first(), target.second()); 
+//			if(kingAttacks.size() == 0)
+//				return true;
+//			Vector<Vector<Tuple<Integer, Integer>>> totalAttackableArea = getTotalAttackableArea(turn);
+//			for(Vector<Tuple<Integer, Integer>> v : kingAttacks) {
+//				Tuple<Integer, Integer> t = v.firstElement();
+//				for(Vector<Tuple<Integer, Integer>> a : totalAttackableArea) {
+//					
+//				}
+//			}
+//			if(totalAttackableArea.contains(getAreaToAttack(PieceType.king, opposite, target.first(), target.second())));
+//			/*
+//			 * 1. 상대편 왕이 갈 자리가 없을 때
+//			 * 2. 상대편 왕이 움직인 후에도 현재 팀의 공격 범위에 있을 때
+//			 * 3. 현재 상대팀 왕을 공격할 수 있는 위치에 있는 현재 팀 말을 상대팀이 공격해서 제거하기가 불가능할 때
+//			 * 4. 왕을 공격할 수 있는 말들의 경로를 막는다.
+//			 */
+			return true; // TODO
+		}
+		public Tuple<Integer, Integer> getTarget(PieceType type, PlayerColor color) {
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					Piece target = chessBoardStatus[j][i];
+					if(target.color == color && target.type == PieceType.king) {
+						return new Tuple<Integer, Integer>(i, j);
+					}
+				}
+			}
+			return null;
 		}
 		public void checkStatus(PieceType type, PlayerColor color, int x, int y) {
 			markPossibleMoves(type, color, x, y);
@@ -246,6 +359,199 @@ public class ChessBoard {
 			Piece target = getIcon(xPrev, yPrev);
 			setIcon(xPrev, yPrev, new Piece());
 			setIcon(x, y, target);
+		}
+		public Vector<Vector<Tuple<Integer, Integer>>> getAreaToAttack(PieceType type, PlayerColor color, int x, int y, PlayerColor turnColor) {
+			System.out.println("getAreaToAttack");
+//			boolean isMoveAttackSame = true; // Set to true if attack range == move range
+//			boolean jump = false; // Set to true for Knight
+			Vector<Vector<Tuple<Integer, Integer>>> attackableArea = new Vector<Vector<Tuple<Integer, Integer>>>();
+			if(type == PieceType.pawn) {
+				//isMoveAttackSame = false;
+				if(color == PlayerColor.black) {
+					Vector<Tuple<Integer, Integer>> temp1 = new Vector<Tuple<Integer, Integer> >();
+					Vector<Tuple<Integer, Integer>> temp2 = new Vector<Tuple<Integer, Integer> >();
+					if(isAttackable(x+1, y+1)) temp1.addElement(new Tuple<Integer, Integer>(x+1, y+1));
+					if(isAttackable(x+1, y-1)) temp2.addElement(new Tuple<Integer, Integer>(x+1, y-1));
+					System.out.println("--" + temp1.size() + "--");
+					attackableArea.addElement(temp1);
+					attackableArea.addElement(temp2);
+				}
+				else if(color == PlayerColor.white) {
+					Vector<Tuple<Integer, Integer>> temp1 = new Vector<Tuple<Integer, Integer>>();
+					Vector<Tuple<Integer, Integer>> temp2 = new Vector<Tuple<Integer, Integer> >();
+					if(isAttackable(x-1, y+1)) temp1.addElement(new Tuple<Integer, Integer>(x-1, y+1));
+					if(isAttackable(x-1, y-1)) temp2.addElement(new Tuple<Integer, Integer>(x-1, y-1));
+					System.out.println("--" + temp1.size() + "--");
+					attackableArea.addElement(temp1);
+					attackableArea.addElement(temp2);
+				}
+			}
+			else if(type == PieceType.rook) {
+				Vector<Tuple<Integer, Integer>> temp1 = new Vector<Tuple<Integer, Integer> >();
+				Vector<Tuple<Integer, Integer>> temp2 = new Vector<Tuple<Integer, Integer> >();
+				Vector<Tuple<Integer, Integer>> temp3 = new Vector<Tuple<Integer, Integer> >();
+				Vector<Tuple<Integer, Integer>> temp4 = new Vector<Tuple<Integer, Integer> >();
+				for(int i = x-1; i >= 0; i--) temp1.addElement(new Tuple<Integer, Integer>(i, y));
+				attackableArea.add(temp1);
+				for(int i = x+1; i < 8; i++) temp2.addElement(new Tuple<Integer, Integer>(i, y));
+				attackableArea.add(temp2);
+				for(int i = y-1; i >= 0; i--) temp3.addElement(new Tuple<Integer, Integer>(x, i));
+				attackableArea.add(temp3);
+				for(int i = y+1; i < 8; i++) temp4.addElement(new Tuple<Integer, Integer>(x, i));
+				attackableArea.add(temp4);
+			}
+			else if(type == PieceType.knight) {
+				//jump = true;
+				Vector<Tuple<Integer, Integer>> temp = new Vector<Tuple<Integer, Integer>>();
+				temp.addElement(new Tuple<Integer, Integer>(x-2, y-1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x-1, y-2));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x-2, y+1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x-1, y+2));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x+2, y+1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x+1, y+2));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x+2, y-1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x+1, y-2));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+			}
+			else if(type == PieceType.bishop) {
+				Vector<Tuple<Integer, Integer>> temp1 = new Vector<Tuple<Integer, Integer> >();
+				Vector<Tuple<Integer, Integer>> temp2 = new Vector<Tuple<Integer, Integer> >();
+				Vector<Tuple<Integer, Integer>> temp3 = new Vector<Tuple<Integer, Integer> >();
+				Vector<Tuple<Integer, Integer>> temp4 = new Vector<Tuple<Integer, Integer> >();
+				for(int i = 1; i < 7; i++) temp1.addElement(new Tuple<Integer, Integer>(x+i, y+i));
+				attackableArea.add(temp1);
+				for(int i = 1; i < 7; i++) temp2.addElement(new Tuple<Integer, Integer>(x+i, y-i));
+				attackableArea.add(temp2);
+				for(int i = 1; i < 7; i++) temp3.addElement(new Tuple<Integer, Integer>(x-i, y+i));
+				attackableArea.add(temp3);
+				for(int i = 1; i < 7; i++) temp4.addElement(new Tuple<Integer, Integer>(x-i, y-i));
+				attackableArea.add(temp4);
+			}
+			else if(type == PieceType.queen) {
+				Vector<Vector<Tuple<Integer, Integer>>> result = new Vector<Vector<Tuple<Integer, Integer>>>();
+				result = getAreaToAttack(PieceType.rook, color, x, y, turnColor);
+				result.addAll(getAreaToAttack(PieceType.bishop, color, x, y, turnColor));
+				return result;
+			}
+			else if(type == PieceType.king) {
+				//jump = true;
+				Vector<Tuple<Integer, Integer>> temp = new Vector<Tuple<Integer, Integer>>();
+				temp.addElement(new Tuple<Integer, Integer>(x-1, y-1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x-1, y));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x-1, y+1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x, y-1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x, y+1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x+1, y-1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x+1, y));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+				temp.addElement(new Tuple<Integer, Integer>(x+1, y+1));
+				attackableArea.add((Vector<Tuple<Integer, Integer>>) temp.clone()); temp.clear();
+			}
+			return getPositionsInRange(attackableArea, false, turnColor);
+		}
+		public Vector<Vector<Tuple<Integer, Integer>>>
+		getPositionsInRange(Vector<Vector<Tuple<Integer, Integer>>> attackableArea, boolean moveOnly, PlayerColor color) {
+			Vector<Vector<Tuple<Integer, Integer>>> attackableAreaInRange = new Vector<Vector<Tuple<Integer, Integer>>>();
+			System.out.println("------ attackableArea should be 2 ---- : " + attackableArea.size());
+			for(Vector<Tuple<Integer, Integer>> v : attackableArea) {
+				Vector<Tuple<Integer, Integer>> temp = new Vector<Tuple<Integer, Integer>>();
+				for(Tuple<Integer, Integer> t : v) {
+					int x = t.first();
+					int y = t.second();
+					if(isInRange(x, y) 
+							&& (getIcon(x, y).type != PieceType.none)) {
+						if(getIcon(x, y).color == color) {
+							break;
+						}
+						else if(getIcon(x, y).color != color) {
+							if(!moveOnly) {
+								temp.addElement(new Tuple<Integer, Integer>(x, y));
+							}
+							break;
+						}
+					}
+					else if(isInRange(x, y)
+							&& (getIcon(x, y).type == PieceType.none)) {
+						temp.addElement(new Tuple<Integer, Integer>(x, y));
+					}
+				}
+				attackableAreaInRange.addElement(temp);
+			}
+			System.out.println("------ should be 2 ---- : " + attackableAreaInRange.size());
+			return attackableAreaInRange;			
+		}
+		public void mark(Vector<Vector<Tuple<Integer, Integer>>> area) {
+			System.out.println("mark");
+			for(Vector<Tuple<Integer, Integer>> v : area) {
+				System.out.println("first for loop : " + v.size());
+				for(Tuple<Integer, Integer> t : v) {
+					int x = t.first();
+					int y = t.second();
+					System.out.println("second for loop : " + x + " " + y);
+					if(isInRange(x, y)) {
+						markPosition(x, y);
+						System.out.println("MarkPostionInRange " + getIcon(x, y).color + getIcon(x,y).type);
+//						if(checkTest && getIcon(x, y).type == PieceType.king) {
+//							isCheck = true;
+//							System.out.println("CHECK!!");
+//						}
+					}
+				}
+			}
+		}
+		public Vector<Vector<Tuple<Integer, Integer>>> getAreaToMove(PieceType type, PlayerColor color, int x, int y, PlayerColor turnColor) {
+			System.out.println("getAreaToMove");
+			Vector<Vector<Tuple<Integer, Integer>>> result = new  Vector<Vector<Tuple<Integer, Integer>>>();
+			Vector<Tuple<Integer, Integer>> movableArea = new Vector<Tuple<Integer, Integer> >();
+			if(type == PieceType.pawn) {
+				if(color == PlayerColor.black) {
+					movableArea.addElement(new Tuple<Integer, Integer>(x+1, y));
+					if(x == 1)
+						movableArea.addElement(new Tuple<Integer, Integer>(x+2, y));
+				}
+				else if(color == PlayerColor.white) {
+					movableArea.addElement(new Tuple<Integer, Integer>(x-1, y));
+					if(x == 6)
+						movableArea.addElement(new Tuple<Integer, Integer>(x-2, y));
+				}
+				System.out.println("moveableArea : " + movableArea.size());
+				result.addElement(movableArea);
+			}
+			return getPositionsInRange(result, true, turnColor);
+		}
+		void getAreaInrange(Vector<Tuple<Integer, Integer> > coords, boolean isMoveAttackSame, boolean jump) {
+			for(Tuple<Integer, Integer> t : coords) {
+				int x = t.first();
+				int y = t.second();
+				System.out.println("--" + x + y);
+				if(isInRange(x, y) 
+						&& (getIcon(x, y).type != PieceType.none)) {
+					if(!isMoveAttackSame || getIcon(x, y).color == turn) {
+						if(!jump)
+							break;
+					}
+					else if(getIcon(x, y).color != turn) {
+						markPositionInRange(x, y);
+						if(!jump)
+							break;
+					}
+				}
+				else
+					markPositionInRange(x, y);
+			}
 		}
 		public void markPossibleMoves(PieceType type, PlayerColor color, int x, int y) {
 			System.out.println("--Mark Possible Moves--");
@@ -389,21 +695,27 @@ public class ChessBoard {
 	}
 	/* member variables */
 	PlayerColor turn = PlayerColor.black;
+	PlayerColor adversary = PlayerColor.white;
 	boolean isCheck = false;
+	boolean isCheckMate = false;
 	boolean checkTest = false;
 	int xPrev;
 	int yPrev;
+	Vector<Vector<Tuple<Integer, Integer>>> attackablearea;
 	
 	void toggleTurn() {
 		if(turn == PlayerColor.black) {
 			turn = PlayerColor.white;
+			adversary = PlayerColor.black;
 			setStatus("WHITE's TURN");
 		}
 		else {
 			turn = PlayerColor.black;
+			adversary = PlayerColor.white;
 			setStatus("BLACK's TURN");
 		}
 		if(isCheck) addStatus("CHECK");
+		else if(isCheckMate) addStatus("CHECKMATE");
 
 	}
 	void onInitiateBoard(){
